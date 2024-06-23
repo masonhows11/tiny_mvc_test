9eaa59b450e439b1b1369299cc8a171bfc2507ce
mason.hows11 <mason.hows11@gmail.com>
2024-06-23 16:22:05 +0330

    implement redirect back with traits

--- system/traits/redirect.php
+++ system/traits/redirect.php
@@ -3,14 +3,25 @@
 namespace system\traits;
 
 
-trait Redirect{
+trait Redirect
+{
 
-    protected function redirect($url){
+    protected function redirect($url)
+    {
 
-        $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
-        header("Location: ".$protocol.$_SERVER['HTTP_HOST']."/tinymvc/".$url);
-
+        $protocol = stripos($_SERVER['SERVER_PROTOCOL'], 'https') === true ? 'https://' : 'http://';
+        header("Location: " . $protocol . $_SERVER['HTTP_HOST'] . "/tinymvc/" . $url);
     }
 
 
+    protected function redirectBack()
+    {
+
+        $http_referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
+        if ($http_referer != null) {
+            header("Location: " . $_SERVER['HTTP_REFERER']);
+        } else {
+            echo "route not found";
+        }
+    }
 }
