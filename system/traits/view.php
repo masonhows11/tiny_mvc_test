604344b4b9d79bc9df27c21a4905ed5b06b9205d
mason.hows11 <mason.hows11@gmail.com>
2024-06-23 22:22:37 +0330

    view trait implemented

--- system/traits/view.php
+++ system/traits/view.php
@@ -1,3 +1,24 @@
 <?php
 
+namespace system\traits;
 
+
+trait View
+{
+
+
+    protected function view(string $dir, $data  = null)
+    {
+        $dir = str_replace('.', '/', $dir);
+        if ($data) {
+
+            extract($data);
+            $path = realpath(dirname(__FILE__) . "/../../application/view/" . $dir . ".php");
+            if (\file_exists($dir)) {
+                return require_once($path);
+            } else {
+                echo "the view [".$dir."] not found";
+            }
+        }
+    }
+}
