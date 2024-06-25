2679fdf1dc0bea8904dc8332de2ba58bb2775b03
mason.hows11 <mason.hows11@gmail.com>
2024-06-25 09:03:52 +0330

    add crud methods to article & category controllers

--- application/controllers/Aarticle.php
+++ application/controllers/Aarticle.php
@@ -4,5 +4,37 @@ namespace application\controllers;
 
 class Article extends Controller{
 
+
+
     
+    public function index()
+    {
+        # code...
+    }
+
+    public function create()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function delete()
+    {
+        # code...
+    }
+
 }
--- application/controllers/Category.php
+++ application/controllers/Category.php
@@ -4,7 +4,36 @@ namespace application\controllers;
 
 class Category  extends Controller
 {
+    
+    public function index()
+    {
+        # code...
+    }
 
+    public function create()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function delete()
+    {
+        # code...
+    }
 
     
 }
