13c81a20a14e058cbd4211d7d18a5f4196928d1e
mason.hows11 <mason.hows11@gmail.com>
2024-06-25 10:44:20 +0330

    implement model article updated

--- application/models/Article.php
+++ application/models/Article.php
@@ -2,12 +2,12 @@
 
 namespace application\models;
 
-class Article extends Model{
+class Article extends Model
+{
 
     public function all()
     {
         # code...
-
         $query  = "SELECT * FROM `articles`;";
         $result = $this->query($query)->fetchAll();
         $this->closeConnection();
@@ -18,23 +18,39 @@ class Article extends Model{
     public function find($id)
     {
         # code...
+        $query  = "SELECT * , (SELECT `name` FROM `categories` WHERE `categories`.`id` = `articles`.`cat_id`) as `category` FROM `articles` WHERE id = ? ";
+        $result = $this->query($query, [$id])->fetchAll();
+        $this->closeConnection();
+        return $result;
     }
 
 
-    public function insert($id)
+    public function insert($values)
     {
         # code...
+        $query  = "INSERT INTO `articles` (`title` , `cat_id` , `body` , created_at) VALUES (?,?,?,now())";
+        $this->execute($query, array_values($values));
+        $this->closeConnection();
     }
 
-    public function update($id,$values)
+    public function update($id, $values)
     {
         # code...
+        $query  = "UPDATE `articles` SET `title` = ? , `cat_id` = ? , `body` = ?  , `updated_at` = now() , WHERE `id` = ? ";
+
+        $this->execute($query, array_merge(array_values($values), [$id]));
+
+        $this->closeConnection();
     }
 
     public function delete($id)
     {
         # code...
+
+        $query  = "DELETE FROM `articles` WHERE `id` = ? ";
+
+        $this->execute($query, [$id]);
+
+        $this->closeConnection();
     }
-
-
 }
